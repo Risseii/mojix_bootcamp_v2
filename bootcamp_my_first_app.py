7f4316a112f3cf1d03e9a8b3e18b6395d9e01e6b
Vania Belén <vany@MacBook-Pro-de-Vania.local>
2022-08-11 19:32:25 -0500

    commit

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 st.write('Hello from the Mojix Bootcamp')
+result = st.button('Click here')
+if result:
+    st.write(':smile:')
 
 st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
 st.write('Simple but effective tips for every python lovers')
+
+st.markdown(body, unsafe_allow_html=False)
+
+from PIL import Image
+image = Image.open('https://miro.medium.com/max/1400/1*5IFgojJ4nU8f0YKTcjWDrg.jpeg')
+st.image(image, caption='Photo by Miesha Maiden from Pexels')
+
+st.markdown(string = “hello world”
+string.split())
