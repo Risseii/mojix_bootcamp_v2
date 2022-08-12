9d0cc43c83c870d788161f281b47132991ab042e
Vania Belén <vany@MacBook-Pro-de-Vania.local>
2022-08-11 19:40:58 -0500

    commit

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -7,7 +7,7 @@ if result:
 st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
 st.write('Simple but effective tips for every python lovers')
 
-st.markdown(body, unsafe_allow_html=False)
+st.markdown('Probando markdown')
 
 from PIL import Image
 image = Image.open('https://miro.medium.com/max/1400/1*5IFgojJ4nU8f0YKTcjWDrg.jpeg')
