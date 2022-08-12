fa7971a1aad2b85ba8d99f5beda197294521db23
Vania Belén <vany@MacBook-Pro-de-Vania.local>
2022-08-11 19:16:52 -0500

    commit

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,2 +1,5 @@
 import streamlit as st
 st.write('Hello from the Mojix Bootcamp')
+
+st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
+st.write('Simple but effective tips for every python lovers')
