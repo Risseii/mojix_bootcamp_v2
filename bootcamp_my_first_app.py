f9dd3cd366af4edbaf25464f1d865cc053fc7e22
Vania Belén <vany@MacBook-Pro-de-Vania.local>
2022-08-11 20:49:51 -0500

    commit

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -13,19 +13,15 @@ st.write('The compactness of Python can make a developer’s life a lot easier w
 
 st.write('In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.')
 
-st.markdown('''
-**1. Walrus operator**
-The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
-\n **_Example_**
-If we want to check and print the length of a list:
-```
-Mylist = [1,2,3]
+st.subheader('**1. Walrus operator**')
+st.write('The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
+st.subheader('**_Example_**')
+code = '''Mylist = [1,2,3]
 if(l := len(mylist) > 2)
-    print(l)
-```     
-''')
+    print(l)''' 
+st.code(code,language = 'python')
 
-st.subheader = '**_Output_**'
+st.subheader('**_Output_**')
 result = '''3'''
 st.code(result,language = 'python')
 
