27ee0723727ca64c399991f7cea1d6c51e6450e7
Vania Belén <vany@MacBook-Pro-de-Vania.local>
2022-08-11 20:18:31 -0500

    commit

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -7,17 +7,17 @@ if result:
 st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
 st.write('Simple but effective tips for every python lovers')
 
-st.markdown("""
-## 1. Walrus operator
+st.markdown('
+**1. Walrus operator**
 The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
-### Example
+**_Example_**
 If we want to check and print the length of a list:
 ```
 Mylist = [1,2,3]
 if(l := len(mylist) > 2)
     print(l)
 ```     
-""")
+')
 
 
 
